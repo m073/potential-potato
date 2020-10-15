889c8bb308a234950b485381d808700c907ab915
m073 <bevan.murray@teamsassist.com>
2020-10-15 16:47:59 +0100

    commit

---
+++ test1.ps1
@@ -0,0 +1,6 @@
+#install microsoft teams
+Install-Module MicrosoftTeams
+#launch sign in window define credential as user input
+$credential = Get-Credential
+#connect to microsoft teams
+Connect-MicrosoftTeams -Credential $credential
